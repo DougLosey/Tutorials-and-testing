e315c93b10f97870fc1e3301eaa1087d2324e74b
Doug <snwbrder590@yahoo.com>
2014-11-10 11:59:40 -0500

    ng-click, ng-repeat, and ng-submit
    Extremely basic functionality, not yet connected to database. uses
    factory.

--- tutorial/client/app/personnel.js
+++ tutorial/client/app/personnel.js
@@ -1,16 +1,43 @@
 angular.module('personnel',[])
+.factory("people", [function() {
+	var o = {
+		people: [
+			{title:'person1', Occupation:'Jr dev'},
+			{title:'person2', Occupation:'Sr dev'},
+			{title:'person3', Occupation:'Delivery Manager'}
+		]
+	};
+	return o;
+}])
 .controller('MainCtrl', [
 	'$scope',
-	function($scope){
+	'people',
+	function($scope, people){
 		$scope.test = 'personnelCreation';
 	
-$scope.people = [
-	{title:'person1', Occupation:'Jr dev'},
-	{title:'person2', Occupation:'Sr dev'},
-	{title:'person3', Occupation:'Delivery Manager'}
-	];
-	
-	}]);
-//$scope.addPerson = function(){
-//	$scope.posts.push({title: 'A new Employee!', Occupation: 'Trainee'});
-//}
+		 $scope.people = people.people;
+						
+		$scope.addPerson = function(){
+			if(!$scope.title || $scope.title === ''){return;}
+			$scope.people.push({title: $scope.title, Occupation: 'Trainee'});
+			$scope.title = '';
+		}
+		$scope.promote = function(person){
+			if (person.Occupation == "Trainee")
+				person.Occupation = "Jr dev";
+			else if (person.Occupation == "Jr dev")
+				person.Occupation = "Sr dev";
+			else if (person.Occupation == "Sr dev")
+				person.Occupation = "Delivery Manager"
+		}
+				$scope.demote = function(person){
+			if (person.Occupation == "Jr dev")
+				person.Occupation = "Trainee";
+			else if (person.Occupation == "Sr dev")
+				person.Occupation = "Jr dev";
+			else if (person.Occupation == "Delivery Manager")
+				person.Occupation = "Sr dev"
+		}
+
+
+}]);
